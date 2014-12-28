9513862ebcfd49c153739c0c8c1db687a91872b4
LeProjetDeLaSemaine <leprojetdelasemaine@gmail.com>
2014-12-28 18:25:56 +0100

    Troisième commit
    Possibilité de typer les arguments d'une fonction/methode

--- PyStatic.py
+++ PyStatic.py
@@ -2,20 +2,36 @@
 """module PyStatic permettant d'utiliser du typage statique en Python"""
 from types import *
 
-def static(cls):
-    """Décrit un décorateur qui permet de définir statiquement une fonction"""
-    """    cls:type du renvoi"""
+def staticArgs(*t):
+    """décrit un décorateur permettant de définir le type des arguments passés à une fonction"""
+    """   *t:tuple contenant les types des variables (dans l'ordre)"""
+    """   0 ignore l'argument actuel et passe au suivant"""
     def wrapper(fonction):
         def sousWrapper(*args,**kwargs):
+            for el in range(len(args)):
+                if(t[el]==0):
+                    pass
+                elif (type(args[el])!= t[el]):
+                    raise TypeError("type of {0} is not {1}".format(args[el],t[el]))
+            fonction(*args,**kwargs)
+        return sousWrapper
+    return wrapper
+                    
+    
+def staticFunc(cls):
+    """Décrit un décorateur qui permet de définir statiquement une fonction"""
+    """    cls:type du renvoi"""
+    def Wrapper(fonction): #notez l'originalité du nom
+        def wrapper(*args,**kwargs): #quelle nom bien trouvé!!!
             if(type(fonction(*args,**kwargs)) is cls):
                 fonction(*args,**kwargs)
             else:
                 raise TypeError("type of {0} is not {1}".format(fonction(*args,**kwargs),cls))
-        return sousWrapper
-    return wrapper
+        return wrapper
+    return Wrapper
 
-
-@static(bool)
+@staticArgs(type,str,0)
+@staticFunc(bool)
 def let(cls,name,value):
     """Assigne statiquement une valeur à une variable"""
     """    cls: type de la variable"""
@@ -31,8 +47,8 @@ def let(cls,name,value):
         raise TypeError("type of {0} is not {1}".format(value,cls))
         return False
         
-
-
-    
+help(let)
+let(int,2)
+print(a)   
         
 
