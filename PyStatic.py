04d3cf8a8752f7c08b8a568742d558935223f46b
LeProjetDeLaSemaine <leprojetdelasemaine@gmail.com>
2014-12-28 15:02:47 +0100

    Premier Commit
    Qui n'a jamais rêvé de faire du typage statique en python???
    Grâce à Super Foussard Entertainment, c'est maintenant possible grâce à
    la fonction "let" et le décorateur "static".

---
+++ PyStatic.py
@@ -0,0 +1,37 @@
+#-*- coding:utf-8 -*-
+"""module PyStatic permettant d'utiliser du typage statique en Python"""
+from types import *
+
+def static(cls):
+    """Décrit un décorateur qui permet de définir statiquement une fonction"""
+    """    cls:type du renvoi"""
+    def wrapper(fonction):
+        def sousWrapper(*args,**kwargs):
+            if(type(fonction(*args,**kwargs)) is cls):
+                fonction(*args,**kwargs)
+            else:
+                raise TypeError("type of {0} is not {1}".format(fonction(*args,**kwargs),cls))
+        return sousWrapper
+    return wrapper
+
+
+@static(bool)
+def let(cls,name,value):
+    """Assigne statiquement une valeur à une variable"""
+    """    cls: type de la variable"""
+    """    name: nom de la variable"""
+    """    value: valeur de la variable"""
+    if(type(value) is cls):
+        if(type(value) is FunctionType):
+            globals()[name] = value
+        else:
+            globals()[name] = cls(value)
+        return True
+    else:
+        return False
+        raise TypeError("type of {0} is not {1}".format(value,cls))
+
+
+    
+        
+
